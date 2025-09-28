d2364a1c4f8bff3fcc4a291b39651f2bf98afb0c
Marie <marie.roche32@orange.fr>
2025-09-28 21:39:23 +0200

    [ADD] adding Movie component

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css';
 import "antd/dist/antd.css";
 import { Button, Popover } from "antd";
+import Movies from './Movies';
 
 function Home() {
 
@@ -17,13 +18,18 @@ const popoverContent = (
     <div className={styles.main}>
       <div className={styles.toppart}>
       <img src='https://cdn.vectorstock.com/i/1000v/00/61/neon-cinema-night-logo-vector-21560061.jpg' alt="Logo" height='100px' width='100px' />
-      <div className={styles.title}>My Movies</div>
+      <div className={styles.title}>MY <span className={styles.span}>MOVIES</span></div>
       <Popover
         title='Liked movies'
         trigger="click"
-        content={popoverContent}>
+        content={popoverContent}
+        className={styles.popover}>
         <Button>4 movie(s)</Button>
       </Popover>
+      </div>
+      <div className={styles.annonce}>
+        LATEST RELEASES !
+        <Movies/>
       </div>
     </div>
   );
--- components/Movies.js
+++ components/Movies.js
@@ -1,16 +1,18 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import React from 'react'
 import styles from '../styles/Movies.module.css';
 
 
-export class Movies extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Movies</Text>
-      </View>
-    )
-  }
+function Movies() {
+  return (
+    <div className={styles.movieCard}>
+        <img className={styles.image} alt='poster' src='https://www.photosmurales.fr/media/catalog/product/cache/3/thumbnail/9df78eab33525d08d6e5fb8d27136e95/import/api-v1.1-file-public-files-pim-assets-97-ad-84-62-6284ad972eff292d45ce1a2e-images-d6-63-ba-65-65ba63d65b4bee91324cbdfd-vd-046-p.jpg'></img>
+        <div className={styles.text}>
+            <span className={styles.name}>name</span>
+            <p className={styles.description}>description</p>
+            <span className={styles.vote}>vote</span>
+        </div>
+    </div>
+  )
 }
 
 export default Movies
