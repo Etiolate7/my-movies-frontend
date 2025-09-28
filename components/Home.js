148bf457472cc08fe6e348800f8b9d48b179659e
Marie <marie.roche32@orange.fr>
2025-09-28 22:19:24 +0200

    [ADD] movies details

--- components/Home.js
+++ components/Home.js
@@ -5,31 +5,38 @@ import Movies from './Movies';
 
 function Home() {
 
-const popoverContent = (
-  <div className={styles.popoverContent}>
-    <span>Movie 1</span>
-    <span>Movie 1</span>
-    <span>Movie 1</span>
-    <span>Movie 1</span>
-  </div>
-)
+  const movies = [];
+  for (let i = 0; i < 10; i++) {
+    movies.push(<Movies />)
+  }
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>
+      <span>Movie 1</span>
+      <span>Movie 1</span>
+      <span>Movie 1</span>
+      <span>Movie 1</span>
+    </div>
+  )
 
   return (
     <div className={styles.main}>
       <div className={styles.toppart}>
-      <img src='https://cdn.vectorstock.com/i/1000v/00/61/neon-cinema-night-logo-vector-21560061.jpg' alt="Logo" height='100px' width='100px' />
-      <div className={styles.title}>MY <span className={styles.span}>MOVIES</span></div>
-      <Popover
-        title='Liked movies'
-        trigger="click"
-        content={popoverContent}
-        className={styles.popover}>
-        <Button>4 movie(s)</Button>
-      </Popover>
+        <img src='https://cdn.vectorstock.com/i/1000v/00/61/neon-cinema-night-logo-vector-21560061.jpg' alt="Logo" height='100px' width='100px' />
+        <div className={styles.title}>MY <span className={styles.span}>MOVIES</span></div>
+        <Popover
+          title='Liked movies'
+          trigger="click"
+          content={popoverContent}
+          className={styles.popover}>
+          <Button>4 movie(s)</Button>
+        </Popover>
       </div>
       <div className={styles.annonce}>
         LATEST RELEASES !
-        <Movies/>
+        <div className={styles.movieContainer}>
+          {movies}
+        </div>
       </div>
     </div>
   );
--- components/Movies.js
+++ components/Movies.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import styles from '../styles/Movies.module.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
 
 
 function Movies() {
+
+const stars = [];
+
+for (let i=0; i < 10; i++) {
+    stars.push(<FontAwesomeIcon key={i} icon={faStar}/>)
+}
+
   return (
     <div className={styles.movieCard}>
         <img className={styles.image} alt='poster' src='https://www.photosmurales.fr/media/catalog/product/cache/3/thumbnail/9df78eab33525d08d6e5fb8d27136e95/import/api-v1.1-file-public-files-pim-assets-97-ad-84-62-6284ad972eff292d45ce1a2e-images-d6-63-ba-65-65ba63d65b4bee91324cbdfd-vd-046-p.jpg'></img>
         <div className={styles.text}>
             <span className={styles.name}>name</span>
             <p className={styles.description}>description</p>
-            <span className={styles.vote}>vote</span>
+            <span className={styles.vote}>{stars}vote</span>
         </div>
     </div>
   )
