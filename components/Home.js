9caa3c208cb2d0c580caf2df11b3738f69b30f13
Marie <marie.roche32@orange.fr>
2025-09-29 18:50:14 +0200

    [ADD] api fetch

--- components/Home.js
+++ components/Home.js
@@ -2,50 +2,62 @@ import styles from '../styles/Home.module.css';
 import "antd/dist/antd.css";
 import { Button, Popover } from "antd";
 import Movies from './Movies';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
- const moviesData = [
-    {title: 'Forrest Gump', poster: 'https://static.wikia.nocookie.net/wikidoublage/images/a/a8/Forrest_Gump_-_Affiche_VOD.jpg/revision/latest/scale-to-width-down/2000?cb=20231122113919&path-prefix=fr', voteAverage:9.2, voteCount: 23_756, overview: "hsvhcjhdvjhv" },
-    {title: '8 Mile', poster: 'https://m.media-amazon.com/images/M/MV5BMzFhZDhjMDAtZWJlZS00ZWUyLWFlZGMtYTcwZjFlODcyMGE2XkEyXkFqcGc@._V1_.jpg', voteAverage:9.2, voteCount: 23_756, overview: "hsvhcjhdvjhv" },
-    {title: 'The Green Mile', poster: 'https://www.rogerebert.com/wp-content/uploads/2024/03/The-Green-Mile.jpg', voteAverage:9.2, voteCount: 23_756, overview: "hsvhcjhdvjhv" },
-    {title: 'Princess Mononoke', poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Princess_Mononoke_Japanese_poster.png/250px-Princess_Mononoke_Japanese_poster.png', voteAverage:9.2, voteCount: 23_756, overview: "hsvhcjhdvjhv" },
-    {title: 'Chihiro', poster: 'https://fr.web.img5.acsta.net/c_310_420/medias/nmedia/00/02/36/71/chihiro.jpg', voteAverage:9.2, voteCount: 23_756, overview: "hsvhcjhdvjhv" }
-  ]
 
 function Home() {
 
-const [likedMovies, setLikedMovies] = useState([]);
+  const [likedMovies, setLikedMovies] = useState([]);
+  const [moviesData, setMoviesData] = useState([]);
 
-const updateLikedMovies = movieTitle => {
+  useEffect(() => {
+fetch('http://localhost:3000/movies')
+.then(response => response.json())
+.then(data => {
+  const formattedData = data.movies.map((movie) => {
+    const poster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+    let overview = movie.overview;
+    if (overview.length > 200) {
+      overview = overview.substring(0, 200) + "...";
+    }
+    return {title: movie.title, poster, voteAverage: movie.vote_average, voteCount: movie.vote_count, overview,}
+  })
+  setMoviesData(formattedData)
+})
+  }, [])
 
-  const isLiked = likedMovies.find(movie => movie --- movieTitle);
+  const updateLikedMovies = movieTitle => {
 
-  if (isLiked) {
-    setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
-  } else {
-    setLikedMovies([...likedMovies, movieTitle]);
+    const isLiked = likedMovies.find(movie => movie-- - movieTitle);
+
+    if (isLiked) {
+      setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
+    } else {
+      setLikedMovies([...likedMovies, movieTitle]);
+    }
   }
-}
+
+
 
   const movies = moviesData.map((data, i) => {
 
-const isLiked = likedMovies.find(movie => movie === data.title)
+    const isLiked = likedMovies.find(movie => movie === data.title)
 
-    return(
+    return (
       <Movies key={i}
-      title={data.title}
-      overview={data.overview}
-      poster={data.poster}
-      voteCount={data.voteCount}
-      voteAverage={data.voteAverage}
-      updateLikedMovies={updateLikedMovies}
-      isLiked={isLiked}/>
+        title={data.title}
+        overview={data.overview}
+        poster={data.poster}
+        voteCount={data.voteCount}
+        voteAverage={data.voteAverage}
+        updateLikedMovies={updateLikedMovies}
+        isLiked={isLiked} />
     )
   });
   for (let i = 0; i < 10; i++) {
-    movies.push(<Movies key={i}/>)
+    movies.push(<Movies key={i} />)
   }
 
 
@@ -53,7 +65,7 @@ const isLiked = likedMovies.find(movie => movie === data.title)
     return (
       <div key={i} className={styles.likedMoviesContainer}>
         <span className='likedmovies'>{data}</span>
-        <FontAwesomeIcon icon={faCircleXmark} className={styles.crossIcon} onClick={() => updateLikedMovies(data)}/>
+        <FontAwesomeIcon icon={faCircleXmark} className={styles.crossIcon} onClick={() => updateLikedMovies(data)} />
       </div>
     )
   })
