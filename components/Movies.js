94cd4c5df158eb935a7a0a08b1bec4f0547c8e0e
Marie <marie.roche32@orange.fr>
2025-09-29 17:22:27 +0200

    [ADD] personal note stars

--- components/Movies.js
+++ components/Movies.js
@@ -2,9 +2,13 @@ import React from 'react'
 import styles from '../styles/Movies.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faStar} from '@fortawesome/free-solid-svg-icons';
-
+import { useState } from 'react';
 
 function Movies(props) {
+
+const [watchCount, setWatchCount] = useState(0);
+const [like, setLike] = useState(false);
+const [note, setNote] = useState(0);
 
 const stars = [];
 
@@ -16,6 +20,19 @@ for (let i=0; i < 10; i++) {
     stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style}/>)
 }
 
+const personalStars = [];
+for (let i = 0; i < 10; i++) {
+  let style = {}
+
+  if (i < note) {
+    style = {color: 'orange'};
+  }
+
+  personalStars.push(
+    <FontAwesomeIcon key={i} icon={faStar} style={style} className='note' />
+  );
+}
+
   return (
     <div className={styles.movieCard}>
         <img className={styles.image} alt={props.title} src={props.poster}></img>
@@ -23,6 +40,9 @@ for (let i=0; i < 10; i++) {
             <span className={styles.name}>{props.title}</span>
             <p className={styles.description}>{props.overview}</p>
             <span className={styles.vote}>{stars} ({props.voteCount})</span>
+            <span>
+              {personalStars} ({note})
+            </span>
         </div>
     </div>
   )
